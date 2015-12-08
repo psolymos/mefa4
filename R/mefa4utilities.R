1a36d5574d598a5c551e666489b26248e7650593
Peter Solymos <psolymos@gmail.com>
2015-12-08 12:48:32 -0700

    error tells what to do

--- R/mefa4utilities.R
+++ R/mefa4utilities.R
@@ -9,7 +9,7 @@ nonDuplicated <- function(x, y, change.rownames=FALSE, na.rm=FALSE) {
     if (nrow(x) != length(y))
         stop("non matching arguments 'x' and 'y'")
     keep <- !duplicated(y)
-    out <- x[keep,,drop=FALSE]
+    out <- x[keep,]
     if (na.rm) {
         y <- y[keep]
         out <- out[!is.na(y),]
@@ -31,19 +31,23 @@ function(x)
         x <- as(x$xtab, "dgCMatrix")
     if (inherits(x, "matrix"))
         x <- as(x, "dgCMatrix")
+    if (is.null(rownames(x)))
+        stop("missing row names")
+    if (is.null(colnames(x)))
+        stop("missing column names")
     if (inherits(x, "sparseMatrix")) {
         x <- as(x, "dgTMatrix")
         rows <- x@i + 1L
         cols <- x@j + 1L
         y <- x@x
-        out <- data.frame(rows = factor(x@Dimnames[[1]][rows],
-            levels=x@Dimnames[[1]]),
-            cols = factor(x@Dimnames[[2]][cols],
-            levels=x@Dimnames[[2]]),
+        out <- data.frame(rows = factor(x@Dimnames[[1]][rows], 
+            levels=x@Dimnames[[1]]), 
+            cols = factor(x@Dimnames[[2]][cols], 
+            levels=x@Dimnames[[2]]), 
             value = y)
-    } else if (is.list(x) && all(sapply(x, function(z)
+    } else if (is.list(x) && all(sapply(x, function(z) 
         inherits(z, "sparseMatrix")))) {
-        if (!all(sapply(x[-1], function(z)
+        if (!all(sapply(x[-1], function(z) 
             identical(z@Dimnames, x[[1]]@Dimnames))))
             stop("dimnames of list elements must be identical")
         n <- length(x)
@@ -54,10 +58,10 @@ function(x)
             cols[[k]] <- X[[k]]@j + 1L
             y[[k]] <- X[[k]]@x
         }
-        out <- data.frame(rows = factor(x[[1]]@Dimnames[[1]][unlist(rows)],
-            levels=x[[1]]@Dimnames[[1]]),
-            cols = factor(x[[1]]@Dimnames[[2]][unlist(cols)],
-            levels=x[[1]]@Dimnames[[2]]),
+        out <- data.frame(rows = factor(x[[1]]@Dimnames[[1]][unlist(rows)], 
+            levels=x[[1]]@Dimnames[[1]]), 
+            cols = factor(x[[1]]@Dimnames[[2]][unlist(cols)], 
+            levels=x[[1]]@Dimnames[[2]]), 
             segm = as.factor(rep(names(x), sapply(y, length))),
             value = unlist(y))
     } else stop("object class not appropriate")
@@ -65,21 +69,21 @@ function(x)
 }
 
 ## atatch a time stamp to file names etc.
-pasteDate <-
+pasteDate <- 
 function(..., sep = " ", collapse = NULL, sep.date = sep)
 {
     out <- paste(..., sep = sep, collapse = collapse)
     paste(out, Sys.Date(), sep = sep.date)
 }
-paste0date <-
+paste0date <- 
 function(..., collapse = NULL)
 {
     paste0(paste0(..., collapse = collapse), Sys.Date())
 }
 
 ## clear up species names
-nameAlnum <-
-function(x, capitalize=c("asis", "first", "none", "all", "mixed"), collapse=" ")
+nameAlnum <- 
+function(x, capitalize=c("asis", "first", "none", "all", "mixed"), collapse=" ") 
 {
     capitalize <- match.arg(capitalize)
     .capwords <- function(x) {
@@ -93,9 +97,9 @@ function(x, capitalize=c("asis", "first", "none", "all", "mixed"), collapse=" ")
         capitalize <- "mixed"
     }
     f <- switch(capitalize,
-        "asis"=function(x) return(x),
-        "mixed"=capwords,
-        "none"=tolower,
+        "asis"=function(x) return(x), 
+        "mixed"=capwords, 
+        "none"=tolower, 
         "all"=toupper)
     sapply(x, function(z) {
         paste0(f(strsplit(gsub("[^[:alnum:] ]", "", z), " +")[[1]]), collapse=collapse)
