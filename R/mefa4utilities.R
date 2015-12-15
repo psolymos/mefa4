5c66967f342cf728f32e99211613a11ce1d9f9bb
Peter Solymos <psolymos@gmail.com>
2015-12-15 11:00:39 -0700

    compare.sets added

--- R/mefa4utilities.R
+++ R/mefa4utilities.R
@@ -106,3 +106,23 @@ function(x, capitalize=c("asis", "first", "none", "all", "mixed"), collapse=" ")
     }, USE.NAMES = !is.null(names(x)))
 }
 
+## compare sets
+compare.sets <- function(x, y) {
+    x <- as.factor(x)
+    y <- as.factor(y)
+    xl <- levels(x)
+    yl <- levels(y)
+    xa <- levels(droplevels(x))
+    ya <- levels(droplevels(y))
+    lab <- c(xlength=length(xl), ylength=length(yl),
+        intersect=length(intersect(xl, yl)),
+        union=length(union(xl, yl)),
+        xbutnoty=length(setdiff(xl, yl)),
+        ybutnotx=length(setdiff(yl, xl)))
+    act <- c(xlength=length(xa), ylength=length(ya),
+        intersect=length(intersect(xa, ya)),
+        union=length(union(xa, ya)),
+        xbutnoty=length(setdiff(xa, ya)),
+        ybutnotx=length(setdiff(ya, xa)))
+    rbind(labels=lab, unique=act)
+}
