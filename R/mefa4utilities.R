d59bcb89ed34c7e6f1791c80a27b5c828773d14c
Peter Solymos <psolymos@gmail.com>
2016-02-11 11:07:05 -0700

    find_min and find_max added

--- R/mefa4utilities.R
+++ R/mefa4utilities.R
@@ -107,7 +107,7 @@ function(x, capitalize=c("asis", "first", "none", "all", "mixed"), collapse=" ")
 }
 
 ## compare sets
-compare.sets <- function(x, y) {
+compare_sets <- function(x, y) {
     x <- as.factor(x)
     y <- as.factor(y)
     xl <- levels(x)
@@ -125,4 +125,26 @@ compare.sets <- function(x, y) {
         xbutnoty=length(setdiff(xa, ya)),
         ybutnotx=length(setdiff(ya, xa)))
     rbind(labels=lab, unique=act)
+}
+
+## finc max index and value for rows of a matrix
+find_max <- function(x) {
+    if (is.null(dim(x)))
+        stop("x must be matrix like object with dim attribute")
+    if (is.null(colnames(x)))
+        colnames(x) <- paste0("X", seq_len(ncol(x)))
+    tmp <- pbapply(x, 1, function(z) {
+        i <- which.max(z)
+        c(i, z[i])
+    })
+    i <- factor(tmp[1,], levels=seq_len(ncol(x)))
+    levels(i) <- colnames(x)
+    out <- data.frame(index=i, value=tmp[2,])
+    rownames(out) <- rownames(x)
+    out
+}
+find_min <- function(x) {
+    out <- find_max(-1 * x)
+    out$value <- -1 * out$value
+    out
 }
