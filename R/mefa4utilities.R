43f6ab4ed097b969aa6c17ac294d7829d8df8499
Peter Solymos <psolymos@gmail.com>
2016-02-11 11:31:14 -0700

    problem with 0 sums

--- R/mefa4utilities.R
+++ R/mefa4utilities.R
@@ -127,19 +127,20 @@ compare_sets <- function(x, y) {
     rbind(labels=lab, unique=act)
 }
 
-## finc max index and value for rows of a matrix
+## find max/min index and value for rows of a matrix
 find_max <- function(x) {
-    if (is.null(dim(x)))
+    if (is.null(dim(x))) 
         stop("x must be matrix like object with dim attribute")
-    if (is.null(colnames(x)))
+    if (is.null(colnames(x))) 
         colnames(x) <- paste0("X", seq_len(ncol(x)))
     tmp <- pbapply(x, 1, function(z) {
         i <- which.max(z)
-        c(i, z[i])
+        if (length(i))
+            c(i, z[i]) else c(NA, NA)
     })
-    i <- factor(tmp[1,], levels=seq_len(ncol(x)))
+    i <- factor(tmp[1, ], levels = seq_len(ncol(x)))
     levels(i) <- colnames(x)
-    out <- data.frame(index=i, value=tmp[2,])
+    out <- data.frame(index = i, value = tmp[2, ])
     rownames(out) <- rownames(x)
     out
 }
