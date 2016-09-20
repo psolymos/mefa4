e16e7cee834d5e5ab650d4f8d4827add62b64c1e
Peter Solymos <psolymos@gmail.com>
2016-09-20 00:01:11 -0600

    new function: reclass

--- R/mefa4utilities.R
+++ R/mefa4utilities.R
@@ -177,3 +177,24 @@ find_min <- function(x) {
 }
 
 "%notin%" <- function(x, table) !(match(x, table, nomatch = 0) > 0)
+
+reclass <- function (x, map) {
+    if (is.null(dim(map)))
+        stop("map argument must be matrix-like object")
+    if (ncol(map) != 2L)
+        stop("map must have 2 columns")
+    if (!is.factor(x))
+        x <- as.factor(x)
+    if (any(is.na(map)))
+        stop("NA found in map")
+    map <- as.matrix(map)
+    if (storage.mode(map) != "character")
+        storage.mode(map) <- "character"
+    if (any(duplicated(map[,1L])))
+        stop("values in 1st column in map must be unique")
+    if (any(!(levels(x) %in% map[,1L])))
+        stop("levels of x must be all part of map[,1]")
+    levels(x) <- map[match(levels(x), map[,1L]),2]
+    x
+}
+
