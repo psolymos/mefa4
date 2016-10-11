950200aa7a112d1dc4cfe36d028e0eab77695f21
Peter Solymos <psolymos@gmail.com>
2016-10-11 15:43:43 -0600

    redistribute

--- R/mefa4utilities.R
+++ R/mefa4utilities.R
@@ -200,3 +200,27 @@ reclass <- function (x, map, all = FALSE) {
     x
 }
 
+redistribute <- function(x, source, target=NULL) {
+    if (is.null(dim(x)) || length(dim(x)) != 2L)
+        stop("x must have 2 dimensions")
+    if (any(x < 0))
+        warning("negative values in x: results might not be meaningful")
+    i <- if (is.numeric(source))
+        source else which(colnames(x) == source)
+    if (is.null(target)) {
+        target <- seq_len(ncol(x))
+        target <- target[target != i]
+    }
+    j <- if (is.numeric(target))
+        target else match(target, colnames(x))
+    if (any(is.na(j)))
+        stop("target must not lead to NA column indices")
+    if (i %in% j)
+        stop("source must not be part of target")
+    m <- x[,j,drop=FALSE]
+    x[,j] <- m + x[,i] * m / rowSums(m)
+    x[,i] <- 0
+    x
+}
+
+
