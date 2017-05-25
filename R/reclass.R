dc56f77f94a385c1c31dfb182934de275a7d7e10
Peter Solymos <psolymos@gmail.com>
2017-05-25 11:15:57 -0600

    reclass allows NA

--- R/reclass.R
+++ R/reclass.R
@@ -1,21 +1,24 @@
-reclass <- function (x, map, all = FALSE) {
+reclass <-
+function (x, map, all = FALSE, allow_NA = FALSE)
+{
     if (is.null(dim(map)))
         stop("map argument must be matrix-like object")
     if (ncol(map) != 2L)
         stop("map must have 2 columns")
     if (!is.factor(x))
         x <- as.factor(x)
-    if (any(is.na(map)))
-        stop("NA found in map")
-    map <- as.matrix(map)
-    if (storage.mode(map) != "character")
-        storage.mode(map) <- "character"
-    if (any(duplicated(map[,1L])))
+    if (any(is.na(map)) && !allow_NA)
+        stop("NA found in map and not allowed")
+    map <- data.frame(map)
+    map[[1L]] <- as.character(map[[1L]])
+    map[[2L]] <- as.character(map[[2L]])
+    if (any(duplicated(map[[1L]])))
         stop("values in 1st column in map must be unique")
-    if (any(!(levels(x) %in% map[,1L])))
+    if (any(!(levels(x) %in% map[[1]][!is.na(map[[1L]])])))
         stop("levels of x must be all part of map[,1]")
     if (all)
-        levels(x) <- c(levels(x), setdiff(map[,1L], levels(x)))
-    levels(x) <- map[match(levels(x), map[,1L]),2]
+        levels(x) <- c(levels(x),
+            setdiff(map[[1L]][!is.na(map[[1L]])], levels(x)))
+    levels(x) <- map[match(levels(x), map[[1L]]), 2]
     x
 }
